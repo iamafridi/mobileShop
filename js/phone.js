2bfbad878d057f5860dc61c8e17c75c7b4c37c8e
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-09-16 16:31:25 +0600

    added the dynamic API

---
+++ js/phone.js
@@ -0,0 +1,93 @@
+const loadPhone = async (searchText,isShowAll) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  );
+  const data = await res.json();
+  const phones = data.data;
+  // console.log(phones);
+  showPhones(phones,isShowAll);
+};
+
+const showPhones = (phones,isShowAll) => {
+  // console.log(phones);
+
+  //Step-1
+  const phoneContainer = document.getElementById("phone-container");
+  // clear phone container cards before adding new cards
+  phoneContainer.textContent = "";
+
+  //Display Showall button if there are more than 12 phones
+  const showAllPhones = document.getElementById('showAllContainer');
+  if(phones.length > 12 && !isShowAll){
+    showAllPhones.classList.remove('hidden');
+  }
+  else{
+    showAllPhones.classList.add('hidden');
+  }
+
+  //Display only first 10 Phones if not show all
+ if(!isShowAll){
+    phones = phones.slice(0,10);
+ }
+
+
+  phones.forEach((phone) => {
+    console.log(phone);
+
+    //STEP-2=create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card p-4 bg-neutral-100 shadow-xl`;
+
+    //Step-3 : Set Inner Html
+    phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Phone" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.brand}</h2>
+          <p>${phone.phone_name} </p>
+          <div class="card-actions justify-center">
+            <button id="handleShowDetails('$phone.slug')" class="btn btn-primary">Show Details</button>
+          </div>
+        </div> 
+        `;
+        // dynamic id string hoye jai tai quotation er vetore id ta send korsi
+    //Step -4= Apppend Child
+    phoneContainer.appendChild(phoneCard);
+  });
+//   hide loading Spinner 
+toggleHandler(false);
+};
+
+// handle search button
+const handleSearch = (isShowAll) => {
+    toggleHandler(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(searchText);
+  loadPhone(searchText,isShowAll);
+};
+
+const toggleHandler = (isLoading)=>{
+    const toggleLoading = document.getElementById('loading-spinner');
+    if(isLoading){
+        toggleLoading.classList.remove('hidden');
+    }
+    else{
+        toggleLoading.classList.add('hidden');
+    }
+
+}
+
+//show Modal details
+const handleShowDetails =async (id)=>{
+    //loading indivisual data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/{id}`);
+    const data = await res.json();
+
+} 
+
+// handle show all 
+const hangleShowAll = ()=>{
+    handleSearch(true);
+}
+
+loadPhone();
